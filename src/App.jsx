d19d5714265453f865c3ec9ffa8d75734662449d
DinithKumudika <dinithwalpitagama@gmail.com>
2023-03-31 22:50:03 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import reactLogo from './assets/react.svg';
+import './App.css';
+import './index.css';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
